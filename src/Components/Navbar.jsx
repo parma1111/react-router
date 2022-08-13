339a8febae144367ffbce4b20e97ea1033169e71
parma1111 <parmarambuldak800@gmail.com>
2022-08-13 09:36:23 -0700

    react-router

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/services">Services</Link>
+    </div>
+  );
+}
+
+export default Navbar;
